959c1d183192976b466119aca8a9544d3e4e6ad4
Steffen Schneider <steffen.schneider@tum.de>
2017-12-11 12:23:15 +0100

    Loading the data as pandas dataframes

--- convert_bag.py
+++ convert_bag.py
@@ -26,11 +26,16 @@ if __name__ == '__main__':
                     groups = match.groups()
                     if len(groups) == 2:
                         key, data = groups
-                        msg_dict[key] = data
+                        if len(data) > 0 and data != '' and data != "''":
+                            msg_dict[key] = json.loads(data)
 
-            msg_dict['topic'] = str(topic)
-            msg_dict['timestamp'] = int(str(t))
-            bag_dict[int(str(t))] = msg_dict
+            #msg_dict['timestamp'] = int(str(t))
+            
+            if not str(topic) in bag_dict.keys():
+                bag_dict[str(topic)] = {}
+                print('New topic {}'.format(str(topic)))
+
+            bag_dict[str(topic)][int(str(t))] = msg_dict
             
             if n % (n_messages // 100) == 0:
                 print('Processing Message {}/{}'.format(n, n_messages))
